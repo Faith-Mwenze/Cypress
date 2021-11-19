9c090cff2955a1192efd11072c454dd390c89784
Faith2662 <mwenzefaith@gmail.com>
2021-11-19 15:32:13 +0300

    Second Commit

--- cypress/integration/Bidding.js
+++ cypress/integration/Bidding.js
@@ -17,6 +17,9 @@ describe("Bidding - normal user ", () => {
         //cancel Bids
         cy.get('[data-testid=MuiDataTableBodyCell-7-0] > :nth-child(2) > .right > .ui').click()
         cy.get('.column > div > .red').click()
+       // cy.screenshot()
+        //cy.screenshot("Bid cancelled succefully")
+      
     
 
 
